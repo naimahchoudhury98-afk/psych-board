cae33e3602d50e21b8fc852308639021a33cb194
Naimah Choudhury <naimah.choudhury98@gmail.com>
2026-02-19 14:07:51 +0000

    set up express server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import pg from "pg";
+
+const app =  express ()
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+const db = new pg.Pool({
+    connectionString: process.env.DB_CONN, 
+});
+
+app.get("/", (req, res) => {
+res.status(200).json({ message: "Welcome to the Psych Board!" });
+});
+
+app.listen(3000, ()=> {
+    console.log("Server runing on http://localhost:3000")
+})
